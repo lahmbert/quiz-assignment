9c6791e816404cf0b8fbbfd0556449d37763321a
Muhammad Sabran <muhammadsabran86.com>
2024-12-28 22:18:31 +0800

    useAuth Fixed

--- app/login/page.js
+++ app/login/page.js
@@ -1,10 +1,12 @@
 'use client';
 
+import { useAuth } from '@/hooks/useAuth';
 import { supabase } from '@/lib/supabaseClient';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 export default function Login() {
+  useAuth();
   const router = useRouter();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
